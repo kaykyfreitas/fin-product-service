12a2804b4e52b67d1d04a7fba0305849479ae1b8
Kayky Freitas <kayky_defreitas@hotmail.com>
2025-06-22 22:08:18 -0300

    feat: add swagger

--- src/main/java/dev/kaykyfreitas/finproductservice/adapter/inbound/api/CategoryAPI.java
+++ src/main/java/dev/kaykyfreitas/finproductservice/adapter/inbound/api/CategoryAPI.java
@@ -2,24 +2,75 @@ package dev.kaykyfreitas.finproductservice.adapter.inbound.api;
 
 import dev.kaykyfreitas.finproductservice.adapter.inbound.api.model.request.CreateCategoryRequest;
 import dev.kaykyfreitas.finproductservice.adapter.inbound.api.model.request.UpdateCategoryRequest;
-import dev.kaykyfreitas.finproductservice.adapter.inbound.api.model.response.CreateCategoryResponse;
-import dev.kaykyfreitas.finproductservice.adapter.inbound.api.model.response.GetCategoryByIdResponse;
-import dev.kaykyfreitas.finproductservice.adapter.inbound.api.model.response.ListCategoryResponse;
-import dev.kaykyfreitas.finproductservice.adapter.inbound.api.model.response.UpdateCategoryResponse;
+import dev.kaykyfreitas.finproductservice.adapter.inbound.api.model.response.*;
 import dev.kaykyfreitas.finproductservice.domain.pagination.Pagination;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Categories")
 @RequestMapping("categories")
 public interface CategoryAPI {
 
+    @Operation(
+            summary = "Create a new category"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Category created successfully"
+                    ),
+                    @ApiResponse(
+                            responseCode = "422",
+                            description = "A validation error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "An internal server error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    )
+            }
+    )
     @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     ResponseEntity<CreateCategoryResponse> create(@RequestBody CreateCategoryRequest request);
 
+    @Operation(
+            summary = "Update an existing category"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Category updated successfully"
+                    ),
+                    @ApiResponse(
+                            responseCode = "422",
+                            description = "A validation error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Category was not found",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "An internal server error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    )
+            }
+    )
     @PutMapping(
             value = "{id}",
             consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -27,12 +78,49 @@ public interface CategoryAPI {
     )
     ResponseEntity<UpdateCategoryResponse> update(@PathVariable String id, @RequestBody UpdateCategoryRequest request);
 
+    @Operation(
+            summary = "Retrieve a category by its identifier"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Category retrieved successfully"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Category or category was not found",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "An internal server error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    )
+            }
+    )
     @GetMapping(
             value = "{id}",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     ResponseEntity<GetCategoryByIdResponse> getById(@PathVariable String id);
 
+    @Operation(
+            summary = "List categories by search params"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Categories retrieved successfully"
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "An internal server error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    )
+            }
+    )
     @GetMapping(
             produces = MediaType.APPLICATION_JSON_VALUE
     )
@@ -44,6 +132,22 @@ public interface CategoryAPI {
             @RequestParam(name = "dir", required = false, defaultValue = "asc") final String direction
     );
 
+    @Operation(
+            summary = "Delete a category by its identifier"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Category deleted successfully"
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "An internal server error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    )
+            }
+    )
     @DeleteMapping(
             value = "{id}"
     )
--- src/main/java/dev/kaykyfreitas/finproductservice/adapter/inbound/api/ProductAPI.java
+++ src/main/java/dev/kaykyfreitas/finproductservice/adapter/inbound/api/ProductAPI.java
@@ -2,24 +2,75 @@ package dev.kaykyfreitas.finproductservice.adapter.inbound.api;
 
 import dev.kaykyfreitas.finproductservice.adapter.inbound.api.model.request.CreateProductRequest;
 import dev.kaykyfreitas.finproductservice.adapter.inbound.api.model.request.UpdateProductRequest;
-import dev.kaykyfreitas.finproductservice.adapter.inbound.api.model.response.CreateProductResponse;
-import dev.kaykyfreitas.finproductservice.adapter.inbound.api.model.response.GetProductByIdResponse;
-import dev.kaykyfreitas.finproductservice.adapter.inbound.api.model.response.ListProductResponse;
-import dev.kaykyfreitas.finproductservice.adapter.inbound.api.model.response.UpdateProductResponse;
+import dev.kaykyfreitas.finproductservice.adapter.inbound.api.model.response.*;
 import dev.kaykyfreitas.finproductservice.domain.pagination.Pagination;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Products")
 @RequestMapping("products")
 public interface ProductAPI {
 
+    @Operation(
+            summary = "Create a new product"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Product created successfully"
+                    ),
+                    @ApiResponse(
+                            responseCode = "422",
+                            description = "A validation error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "An internal server error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    )
+            }
+    )
     @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     ResponseEntity<CreateProductResponse> create(@RequestBody CreateProductRequest request);
 
+    @Operation(
+            summary = "Update an existing product"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Product updated successfully"
+                    ),
+                    @ApiResponse(
+                            responseCode = "422",
+                            description = "A validation error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Product or category was not found",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "An internal server error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    )
+            }
+    )
     @PutMapping(
             value = "{id}",
             consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -27,12 +78,49 @@ public interface ProductAPI {
     )
     ResponseEntity<UpdateProductResponse> update(@PathVariable String id, @RequestBody UpdateProductRequest request);
 
+    @Operation(
+            summary = "Retrieve a product by its identifier"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Product retrieved successfully"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Product or category was not found",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "An internal server error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    )
+            }
+    )
     @GetMapping(
             value = "{id}",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     ResponseEntity<GetProductByIdResponse> getById(@PathVariable String id);
 
+    @Operation(
+            summary = "List products by search params"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Products retrieved successfully"
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "An internal server error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    )
+            }
+    )
     @GetMapping(
             produces = MediaType.APPLICATION_JSON_VALUE
     )
@@ -44,6 +132,22 @@ public interface ProductAPI {
             @RequestParam(name = "dir", required = false, defaultValue = "asc") final String direction
     );
 
+    @Operation(
+            summary = "Delete a product by its identifier"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Products deleted successfully"
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "An internal server error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponse.class))
+                    )
+            }
+    )
     @DeleteMapping(
             value = "{id}"
     )
