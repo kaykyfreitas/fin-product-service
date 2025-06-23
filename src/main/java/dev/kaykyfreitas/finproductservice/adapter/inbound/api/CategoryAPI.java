5b20867cc616371487f85fb9bc31b354292e5842
Kayky Freitas <kayky_defreitas@hotmail.com>
2025-06-22 21:51:38 -0300

    fix: adjust category rest api request mapping

--- src/main/java/dev/kaykyfreitas/finproductservice/adapter/inbound/api/CategoryAPI.java
+++ src/main/java/dev/kaykyfreitas/finproductservice/adapter/inbound/api/CategoryAPI.java
@@ -11,7 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("category")
+@RequestMapping("categories")
 public interface CategoryAPI {
 
     @PostMapping(
